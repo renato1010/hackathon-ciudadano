8ddbd380d645deeeb96f6f4a2f3e886125c77084
Renato Perez <renato.perez@bairesdev.com>
2019-05-16 08:30:40 -0600

    Add AWS Amplify Auth functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+import { AuthState } from 'aws-amplify-angular/dist/src/providers';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-amplify';
+  authState: Observable<AuthState>;
+  estaLogeado: Observable<boolean>;
+  usuario: Observable<any>;
+
+  constructor(private amplifyService: AmplifyService) {
+    this.authState = amplifyService.authStateChange$;
+    this.estaLogeado = this.authState.pipe(map(authState => authState.state === 'signedIn'));
+    this.usuario = this.authState.pipe(map(authState => authState.user));
+  }
 }
